b382b8607076c825e445a33c978ce99aa94d57f6
Sasha Mainardi <sashamainardi4@gmail.com>
2021-12-22 16:16:20 +0100

    finished with condition inside for cicle

--- script.js
+++ script.js
@@ -5,10 +5,23 @@
 // se multiplo di entrambi stampo fizzbuzz 
 // altrimenti stampo numero
 
+// Dichiaro variabile number 
 let number = '';
 
-for (let i=1; i<100; i++){
-    number += `${i}`;
+
+// ciclo da 1 a 100 
+for (let i=1; i<=100; i++){
+    if (( i % 3 ) == 0 && ( i % 5 ) == 0){
+        number += ' FizzBuzz';
+    } else if(( i % 3 ) == 0){
+        number += ' fizz';
+    } else if (( i % 5 ) == 0){
+        number += ' buzz';
+    } else{
+        number += ` ${i}`;
+    }
 }
 
+// stampo risultato console 
 console.log(number);
+
