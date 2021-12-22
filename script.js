e289b392829538a9d5ab548fe6c87b3fc80b3418
Sasha Mainardi <sashamainardi4@gmail.com>
2021-12-22 15:49:24 +0100

    first step

---
+++ script.js
@@ -0,0 +1,14 @@
+// ciclo numeri da 1 a 100 
+// immagazzino numero in una variabile 
+// se numero multiplo di 3 stampo fizz 
+// se multiplo di 5 stampo buzz
+// se multiplo di entrambi stampo fizzbuzz 
+// altrimenti stampo numero
+
+let number = '';
+
+for (let i=1; i<100; i++){
+    number += `${i}`;
+}
+
+console.log(number);
