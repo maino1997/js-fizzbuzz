fe50339c897e82fc0e19a99f7c166d2efedfb730
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-10 10:20:24 +0100

    added =

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ const displayElement = document.getElementById('display');
 
 // ciclo da 1 a 100 
 for (let i=1; i<=100; i++){
-    if (( i % 3 ) == 0 && ( i % 5 ) == 0){
+    if (( i % 3 ) === 0 && ( i % 5 ) === 0){
         number = number + "<li> FizzBuzz </li>"
-        } else if(( i % 3 ) == 0){
-        number = number + "<li> fizz </li>"
-        } else if (( i % 5 ) == 0){
+        } else if(( i % 3 ) === 0){
+        number = number + "<li> Fizz </li>"
+        } else if (( i % 5 ) === 0){
         number = number + "<li> Buzz </li>"
         } else{
         number =number + `<li>${i}</li>`;
